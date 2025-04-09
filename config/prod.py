20669cbf194ec38aae94ce215fc46b5954bd553e
gonmace <magoreal38@gmail.com>
2025-04-08 23:21:38 -0400

    first commit

--- config/prod.py
+++ config/prod.py
@@ -29,8 +29,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT', cast=int)
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', cast=bool)
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', default=False)
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
@@ -52,4 +52,4 @@ CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 
-CSRF_TRUSTED_ORIGINS = ['https://chatlegal.c4all.org.']
+CSRF_TRUSTED_ORIGINS = ['https://chatlegal.c4all.org']
